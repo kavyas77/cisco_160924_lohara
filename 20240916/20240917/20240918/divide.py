fff5d6d11b7fbcb13bbc9da11530eded290b5688
kavyas77 <kavyashanmugavadivel@gmai.com>
2024-09-18 16:20:18 +0530

    Intro to py

---
+++ 20240916/20240917/20240918/divide.py
@@ -0,0 +1,17 @@
+def find_quotient(d,n):
+    if n == 0:
+        raise ZeroDivisionError()
+    return d/n
+
+try:
+    number = int(input("Enter a number: "))
+    result = 10 / number
+except ValueError:
+    print("Invalid input! Please enter a valid integer.")
+except ZeroDivisionError:
+    print("You cannot divide by zero!")
+else:
+    print(result)
+finally:
+    print('cleaning up')
+print('End of program')
