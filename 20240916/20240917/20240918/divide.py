aed46efdb5436430f9d96344145afd61f49dfda6
kavyas77 <kavyashanmugavadivel@gmai.com>
2024-09-19 09:20:33 +0530

    Intro to py

--- 20240916/20240917/20240918/divide.py
+++ 20240916/20240917/20240918/divide.py
@@ -1,14 +1,18 @@
+class MyException(ZeroDivisionError):
+    pass
+
+
 def find_quotient(d,n):
     if n == 0:
-        raise ZeroDivisionError()
+        raise MyException()
     return d/n
 
 try:
     number = int(input("Enter a number: "))
-    result = 10 / number
+    result = find_quotient(10,number)
 except ValueError:
     print("Invalid input! Please enter a valid integer.")
-except ZeroDivisionError:
+except MyException:
     print("You cannot divide by zero!")
 else:
     print(result)
